48401e262de48a636d76889f6a499dba0757bb85
SaiUbc <pusulurs@student.ubc.ca>
2025-01-19 05:27:00 -0800

    added function templates

--- RAG/create_user_response.py
+++ RAG/create_user_response.py
@@ -3,16 +3,16 @@
 
 # TODO's!
 # 
-#  1) LOAD BERT MODEL
-#  2) TOPIC MODEL THE USER PROMPT USING THIS MODEL TO GENEREATE A TOPIC NUMBER    ---- (A)
-#  3) CREATE A SUMMARY OF THE USER PROMPT
-#  4) CREATE A SENTIMENT OF THE USER PROMPT
+#  1) LOAD BERT MODEL     d
+#  2) TOPIC MODEL THE USER PROMPT USING THIS MODEL TO GENEREATE A TOPIC NUMBER    ---- (A) d
+#  3) CREATE A SUMMARY OF THE USER PROMPT                        d
+#  4) CREATE A SENTIMENT OF THE USER PROMPT           d
 #  
 #  RAG STUFF
 # 
 #  PART 1
 #
-#  5) USE THE TOPIC MODEL NUMBER FROM (A) TO QUERY THE 'data/topic_modelled_database/test_data.csv'
+#  5) USE THE TOPIC MODEL NUMBER FROM (A) TO QUERY THE 'data/topic_modelled_database/test_data.csv'      d
 #  6) USE ANGUS SQL SCRIPT TO GET TOP 3 THERAPIST RESPONSES FOR THE USER PROMPT
 # 
 #  PART 2
@@ -22,3 +22,15 @@
 #  8) COMBINE PART 1 AND PART 2 TO GENEREATE A BIG PROMPT
 #
 #  9) PASS INTO THE LLM MODEL TO GENERATE A RESPONSE
+
+
+def load_bert_model():
+    # Load BERT model
+    return 
+
+def topic_model_user_prompt(user_prompt):
+    # Load BERT model
+    # Use BERT model to topic model the user prompt
+    # Return topic number
+    return 1
+
